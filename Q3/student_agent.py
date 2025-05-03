e9d8bcb952dc868a023b8dc6b2be2adf5c5cb1f1
KyleChang110907 <r12521627@ntu.edu.tw>
2025-05-03 18:31:26 +0800

    change path 5

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -18,7 +18,7 @@ from dmc import make_dmc_env  # 你訓練程式裡定義的環境建構
 from Q3.training.SAC.SAC import PolicyNetwork, MODEL_DIR  # 請替換成你訓練檔案的實際路徑
 
 MODEL_DIR = MODEL_DIR.replace('\\Q3', '')
-MODEL_DIR = MODEL_DIR.replace('/best', '\\best')
+# MODEL_DIR = MODEL_DIR.replace('/best', '\\best')
 # ———————— 只做 Inference 的 Agent ————————
 class Agent(object):
     def __init__(self):
@@ -29,7 +29,7 @@ class Agent(object):
         self.policy = PolicyNetwork(obs_dim, act_dim).to(self.device)
 
         # 載入你訓練後最好的 checkpoint
-        ckpt = torch.load(MODEL_DIR+"/best_actor.pth", map_location=self.device)
+        ckpt = torch.load(MODEL_DIR+"\\best_actor.pth", map_location=self.device)
         self.policy.load_state_dict(ckpt)
         self.policy.eval()
 
